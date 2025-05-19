25dbe4ec201c7b7382f41bdc39fe7544b536606e
KyuzenDev <antcraft517@gmail.com>
2025-05-19 10:07:35 +0700

    fix(navbar, footer): correct semantic tag usage and accessibility issues

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,51 +1,51 @@
-import next from "next";
 import Link from "next/link";
 import Image from "next/image";
-import Facebook from 'public/footer/facebook.svg'
-import Instagram from 'public/footer/instagram.svg';
-import Dribbble from 'public/footer/dribbble.svg';
-import Linkedin from '/public/footer/linkedin.svg';
 
 export default function Footer() {
-    return (
-        <footer className="max-w-full w-full h-[222px] px-40 justify-center items-center">
-            <div className="w-238 h-12 gap-40 py-20  bg-transparent flex flex-row justify-center items-center">
-                <div className="w-[113px] h-6">
-                    <img src="Secoliner.svg" alt="Pipeliner-Logos" />
-                </div>
-                <ul className='w-[313px] h-[37px] gap-8 flex flex-row justify-beetwen items-center'>
-                    <li>
-                        <Link href="">Home</Link>
-                    </li>
-                    <li>
-                        <Link href="">About</Link>
-                    </li>
-                    <li>
-                        <Link href="">Pricing</Link>
-                    </li>
-                    <li>
-                        <Link href="">Blog</Link>
-                    </li>
-                    <li>
-                        <Link href="">Contact</Link>
-                    </li>
-                </ul>
-                <div className="w-45 h-6 gap-2 flex flex-row justify-beetwen items-center">
-                    <button className="w-10 h-10 p-2 justify-center items-center rounded-[5px] bg-gray-300 cursor-pointer">
-                        <Image src={Facebook} alt="facebook" />
-                    </button>
-                    <button className="w-10 h-10 p-2 justify-center items-center rounded-[5px] bg-gray-300 cursor-pointer">
-                        <Image src={Instagram} alt="instagram" />
-                    </button>
-                    <button className="w-10 h-10 p-2 justify-center items-center rounded-[5px] bg-gray-300 cursor-pointer">
-                        <Image src={Dribbble} alt="dribbble" />
-                    </button>
-                    <button className="w-10 h-10 p-2 justify-center items-center rounded-[5px] bg-gray-300 cursor-pointer">
-                        <Image src={Linkedin} alt="linkedin" />
-                    </button>
-                </div>
-            </div>
-            <div className=""></div>
-        </footer>
-    )
+  return (
+    <footer className="max-w-full w-full h-[222px] px-40 justify-center items-center">
+      <div className="w-238 h-12 gap-40 py-20  bg-transparent flex flex-row justify-center items-center">
+        <div className="w-[113px] h-6">
+          <Image
+            src="/logo/app-logo.png"
+            alt="App Logo"
+            width={113}
+            height={24}
+          />
+        </div>
+        <ul className="w-[313px] h-[37px] gap-8 flex flex-row justify-beetwen items-center">
+          <li>
+            <Link href="">Home</Link>
+          </li>
+          <li>
+            <Link href="">About</Link>
+          </li>
+          <li>
+            <Link href="">Pricing</Link>
+          </li>
+          <li>
+            <Link href="">Blog</Link>
+          </li>
+          <li>
+            <Link href="">Contact</Link>
+          </li>
+        </ul>
+        <div className="w-45 h-6 gap-2 flex flex-row justify-beetwen items-center">
+          <button className="w-10 h-10 p-2 justify-center items-center rounded-[5px] bg-gray-300 cursor-pointer">
+            <Image src="/footer/facebook.svg" alt="facebook" />
+          </button>
+          <button className="w-10 h-10 p-2 justify-center items-center rounded-[5px] bg-gray-300 cursor-pointer">
+            <Image src="/footer/instagram.svg" alt="instagram" />
+          </button>
+          <button className="w-10 h-10 p-2 justify-center items-center rounded-[5px] bg-gray-300 cursor-pointer">
+            <Image src="/footer/dribble.svg" alt="dribbble" />
+          </button>
+          <button className="w-10 h-10 p-2 justify-center items-center rounded-[5px] bg-gray-300 cursor-pointer">
+            <Image src="/footer/linkedin.svg" alt="linkedin" />
+          </button>
+        </div>
+      </div>
+      <div className=""></div>
+    </footer>
+  );
 }
--- src/components/TopNavbar.tsx
+++ src/components/TopNavbar.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
-import logo from "../../public/logo/app-logo.png";
 import Image from "next/image";
 
 const TopNavbar = () => {
@@ -8,17 +7,22 @@ const TopNavbar = () => {
     <div className="w-full flex items-center justify-between bg-transparent py-5">
       <div className="w-fit px-20 xl:w-[100%] gap-30 bg-transparent flex flex-row justify-between items-center">
         <div>
-          <Image src={logo.src} alt="App Logo" width={113} height={24} />
-        </div>
-        <ul className="gap-8 flex flex-row justify-between items-center">
+          <div>
+            <Image
+              src="/logo/app-logo.png"
+              alt="App Logo"
+              width={113}
+              height={24}
+            />
+          </div>
+          <ul>
             <Link href="#">About</Link>
             <Link href="#">Pricing</Link>
             <Link href="#">Blog</Link>
             <Link href="#">Contact</Link>
-        </ul>
-        <Button>
-          Start for Free
-        </Button>
+          </ul>
+          <Button>Start for Free</Button>
+        </div>
       </div>
     </div>
   );

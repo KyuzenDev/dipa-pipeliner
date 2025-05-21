d53e52b2d8fcee8282e550011c19c6a6bae1684e
Aira Aria <127509175+JihadZaidan@users.noreply.github.com>
2025-05-21 12:58:59 +0700

    Create relationship.ts

---
+++ src/data/relationship.ts
@@ -0,0 +1,23 @@
+export type ContentPages = {
+    header: string;
+    description: string;
+    image: string;
+};
+
+export const contentpages: ContentPages[] = [
+    {
+        header: "Data Powered Workflows Design",
+        description: "Automate your go market decisions based on the most reliable dataset.",
+        image: "/public/landing/Second/grid-one.svg"
+    },
+    {
+        header: "Your Team Synergy Station",
+        description: "Pipeliner boosts go market teams productivity and streamlines tasks.",
+        image: "/public/landing/Second/grid-two.svg"
+    },
+    {
+        header: "Automate with Intelligent Control",
+        description: "Effortlessly automate intricate business processes with our smart.",
+        image: "/public/landing/Second/grid-three.svg"
+    }
+];

cdd68d8532e9ace3848ab58921de2a1dae110dbf
Aira Aria <127509175+JihadZaidan@users.noreply.github.com>
2025-05-21 16:16:37 +0700

    Update page.tsx

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,16 +1,1 @@
-import Twobar from "@/components/layout/Navbar/Twobar"
-import CMSH from "@/components/layout/Header/CMSHeader"
-import Footer from "@/components/layout/Footer/Footer"
-import BlogCard from "@/components/content/cms"
-// import CTA from "@/components/layout/CTA/CTA"
 
-export default function Blog() {
-    return (
-        <section className="max-w- w-full h-[1900px] bg-white flex flex-col justify-between items-center gap-2">
-            <Twobar/>
-            <CMSH/>
-            <BlogCard/>
-            <Footer/>
-        </section>
-    )
-}

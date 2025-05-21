de82920aed3912f672e099c19c60dbec8147d3ad
Aira Aria <127509175+JihadZaidan@users.noreply.github.com>
2025-05-21 14:40:32 +0700

    Update TopNavbar.tsx

--- src/components/TopNavbar.tsx
+++ src/components/TopNavbar.tsx
@@ -1,8 +1,12 @@
+import { useState } from 'react';
 import Link from "next/link";
 import { Button } from "./ui/button";
 import Image from "next/image";
+import { Menu , X } from 'lucide-react'; 
 
 export default function TopNavbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  
   return (
     <div className="w-full max-w-[1520px] mx-auto flex items-center justify-between py-5 px-6 md:px-16">
       <div>
@@ -13,7 +17,9 @@ export default function TopNavbar() {
           height={24}
         />
       </div>
-      <ul className="flex flex-wrap text-grayscale-900 gap-4 md:gap-10 justify-center items-center">
+
+      {/* Dekstop Menu */}
+      <ul className="hiden md:flex flex-wrap text-grayscale-900 gap-4 md:gap-10 justify-center items-center">
         <li>
           <Link href="" className="hover:text-gray-600">
             About
@@ -35,7 +41,48 @@ export default function TopNavbar() {
           </Link>
         </li>
       </ul>
-      <Button>Start for Free</Button>
+
+      {/* CTA Button (Dekstop)*/}
+      <div clasName="hidden md:block">
+        <Button>Start for Free</Button>
+      </div>
+
+      {/* Hamburger Button (Mobile)*/}
+      <button
+        className="md:hidden text-gray-700"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {isOpen ? <X size={24} /> : <Menu size={24} />}
+      </button>
     </div>
+
+     {/* Mobile Menu */}
+      {isOpen && (
+        <div className="md:hidden px-6 pb-4 space-y-4">
+          <ul className="flex flex-col gap-4 text-grayscale-900">
+            <li>
+              <Link href="" className="hover:text-gray-600" onClick={() => setIsOpen(false)}>
+                About
+              </Link>
+            </li>
+            <li>
+              <Link href="" className="hover:text-gray-600" onClick={() => setIsOpen(false)}>
+                Pricing
+              </Link>
+            </li>
+            <li>
+              <Link href="" className="hover:text-gray-600" onClick={() => setIsOpen(false)}>
+                Blog
+              </Link>
+            </li>
+            <li>
+              <Link href="" className="hover:text-gray-600" onClick={() => setIsOpen(false)}>
+                Contact
+              </Link>
+            </li>
+          </ul>
+          <Button className="w-full">Start for Free</Button>
+        </div>
+      )}
   );
 }

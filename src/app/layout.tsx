ae7dea9a3c1ab5458930542e44a1b00c90c27ccd
KyuzenDev <antcraft517@gmail.com>
2025-05-22 09:23:44 +0700

    refactor(files): reorganize project structure for better maintainability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,12 @@
-import type { Metadata } from "next";
+'use client'
 import { Inter_Tight } from "next/font/google";
 import "./globals.css";
+import TopNavbar from "@/components/TopNavbar";
+import Footer from "@/components/Footer";
 
 const inter = Inter_Tight({
   subsets: ["latin"],
 });
-
-export const metadata: Metadata = {
-  title: "Pipeliner Dashboard",
-  description: "A dashboard for Pipeliner",
-};
 
 export default function RootLayout({
   children,
@@ -21,7 +18,17 @@ export default function RootLayout({
       <body
         className={inter.className}
       >
-        {children}
+        <main className="flex flex-col items-center justify-between w-full">
+          <div className="relative w-full h-full">
+            <div className="absolute bg-gradient-custom inset-0 backdrop-blur-md h-screen max-h-[70vh] z-[-10]">
+              <div className="absolute w-full h-full shadow-blur z-[-5]"></div>
+            </div>
+          </div>
+
+          <TopNavbar />
+          <div className="w-full max-w-7xl py-12">{children}</div>
+          <Footer />
+        </main>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
-import PipelinerHomePage from "./pipeliner-homepage/page";
 
+import Features_1 from "@/app/pipeliner-homepage/featuresOne/page";
+import Hero from "@/app/pipeliner-homepage/hero/page";
+import Stats from "@/app/pipeliner-homepage/stats/page";
 
 export default function RootPage() {
   return (
     <>
-      <PipelinerHomePage />
+      <Hero />
+      <Features_1 />
+      <Stats />
     </>
   );
 }

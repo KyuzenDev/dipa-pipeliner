e0ad801b8486126112bf51c1934b071e73c87e5f
KyuzenDev <antcraft517@gmail.com>
2025-05-23 13:32:49 +0700

    fix(jsx): replace unescaped single quotes with &apos; to comply with JSX syntax

--- src/components/blog/blogdetails/contentblogdetail.tsx
+++ src/components/blog/blogdetails/contentblogdetail.tsx
@@ -3,16 +3,16 @@ export default function ContentBlogDetail() {
         <div className="flex w-full justify-center">
             <div className="flex flex-col gap-8 w-full max-w-[720px]">
                 <p className="font-sans text-large font-normal text-gray-500 leading-8 text-left">
-                    In 2025, <u className="text-gray-900">Customer Relationship Management (CRM)</u>is poised for some thrilling
+                    In 2025, <u className="text-gray-900">Customer Relationship Management (CRM)</u>is poised for some thrilling
                     changes. With the rapid rise of generative AI, shifting business dynamics, and an
-                    increasing focus on post-sales support, CRM is evolving to be faster and flexible.
+                    increasing focus on post-sales support, CRM is evolving to be faster and flexible.
                 </p>
                 <div className="flex flex-col w-full h-[132px] gap-4">
                     <h4 className="w-full h-[30px] text-left font-sans font-semibold text-2xl leading-[120%]">
                         The Game Changer For CRM
                     </h4>
                     <p className="w-full h-[87px] text-gray-600 text-left font-sans font-normal text-large leading-[160%]">
-                        By 2025, AI-powered CRMs won’t just store data they’ll write follow-ups, summarize
+                        By 2025, AI-powered CRMs won&apos;t just store data they&apos;ll write follow-ups, summarize
                         meetings, track interactions, and predict customer needs. What was once a bonus
                         will soon be essential. These AI-driven features will shift from nice-to-have.
                     </p>
@@ -28,7 +28,7 @@ export default function ContentBlogDetail() {
                     </h4>
                     <p className="font-sans font-normal text-left text-large leading-[160%] text-gray-900">
                         By 2025, the biggest shift in CRM is the growing focus on the post-sales memory,
-                        CRMs are no longer just for closing deals—they’re evolving to support the entire
+                        CRMs are no longer just for closing deals—they&apos;re evolving to support the entire
                         customer journey, from first touch to long-term retention.
                         <br />
                         • Pipeliner are evolving beyond just sales tools
@@ -37,7 +37,7 @@ export default function ContentBlogDetail() {
                     </p>
                 </div>
                 <p className="font-sans font-normal text-left text-large leading-[160%] text-gray-600">
-                    The future of CRM isn't just about tracking interactions; it's about enhancing those
+                    The future of CRM isnt just about tracking interactions; its about enhancing those
                     interactions through smart, user-friendly, and secure solutions that benefit both
                     businesses and customers.
                 </p>
@@ -46,7 +46,7 @@ export default function ContentBlogDetail() {
                         Smaller Vendors On The Rise
                     </h4>
                     <p className="font-sans font-normal text-left text-large leading-[160%] text-gray-900">
-                        AI isn’t the only force transforming CRM smaller vendors are gaining ground, offering
+                        AI isn&apos;t the only force transforming CRM smaller vendors are gaining ground, offering
                         flexible, user-friendly solutions that rival legacy systems. Once focused on SMBs,
                         these new players are now ready to meet enterprise demands with integration.
                     </p>

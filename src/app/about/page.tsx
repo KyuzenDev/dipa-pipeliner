98b3c16e0a80d7bfea5ecc6288d206d1433bfb04
KyuzenDev <antcraft517@gmail.com>
2025-05-27 11:31:05 +0700

    style(about-guided): enhance UI for guided section with improved typography and layout

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,33 +1,33 @@
 import GuidedCard from "@/components/about/components/guidedCard";
-
+import { Badge } from "@/components/ui/badge";
 
 export default function About() {
     return (
-        <div className="max-w-full w-full h-[741px] py-[104px] px-[64px] gap-2">
-            <div className="w-1072px h-[196px] gap-4 justify-center items-center flex flex-col">
-                <div className="w-[73px] h-[44px] rounded-20 border border-gray-200 px-4 gap-2 bg-white justify-center items-center">
-                    <h6 className="font-sans font-medium text-sm leading-[150%] align-middle text-gray-900 tetx-center">About</h6>
+        <div className="max-w-full w-full h-auto py-8 px-4 md:py-[104px] md:px-[64px] gap-2">
+            <div className="w-full h-auto md:h-[196px] gap-4 justify-center items-center flex flex-col mx-auto">
+                <Badge>
+                    About
+                </Badge>
+                <div className="w-full h-auto justify-center items-center flex flex-col gap-1 text-center">
+                    <h2 className="font-semibold text-xl md:text-4xl leading-[120%] text-grayscale-900">The CRM Behind</h2>
+                    <p className="font-normal text-xl md:text-4xl leading-[120%] text-grayscale-900">Thousands of Companies</p>
                 </div>
-                <div className="w-full h-24 justify-center item-center flex flex-col gap-1">
-                    <h2 className="font-sans font-semibold text-2xl leading-[120%] text-gray-900">The CRM Behind</h2>
-                    <p className="font-sans font-normal text-2xl leading-[120%] text-gray-900">Thousands of Companies</p>
-                </div>
-                <p className="w-full h-[24px] font-sans font-normal text-base leading-[150%] text-gray-700">
+                <p className="font-normal text-sm md:text-base leading-[150%] text-grayscale-700 text-center">
                     Pipeliner is on a mission to craft tools that support business growth through smart.
                 </p>
             </div>
-            <div className="w-[1072px] h-[533px] gap-[64px] flex flex-col justify-between items-center">
-                <div className="w-full h-[148px] gap-4 flex flex-col justify-between items-center">
-                    <div className="w-[73px] h-[44px] rounded-20 border border-gray-200 px-4 gap-2 bg-white justify-center items-center">
-                        <h6 className="font-sans font-medium text-sm leading-[150%] align-middle text-gray-900 tetx-center">About</h6>
-                    </div>
-                    <h2 className="w-full h-12 font-sans font-semibold text-2xl leading-[120%] text-gray-900">Value Driven Actions</h2>
-                    <p className="w-full h-[24px] font-sans font-normal text-base leading-[150%] text-gray-700">
-                        Don’t hesitate to reach out us if you need further assistance.
+            <div className="w-full h-auto gap-8 flex flex-col items-center mx-auto">
+                <div className="w-full h-auto gap-4 flex flex-col justify-between items-center text-center">
+                    <Badge>
+                        Guided
+                    </Badge>
+                    <h2 className="font-semibold text-xl md:text-4xl leading-[120%] text-grayscale-900">Value Driven Actions</h2>
+                    <p className="font-normal text-sm md:text-base leading-[150%] text-grayscale-700">
+                        Don’t hesitate to reach out to us if you need further assistance.
                     </p>
                     <GuidedCard />
                 </div>
             </div>
         </div>
-    )
+    );
 }
--- src/data/guide.ts
+++ src/data/guide.ts
@@ -1,22 +1,25 @@
 import { Guide } from "@/app/about/types/aboutGuided";
 
 export const guide: Guide[] = [
-    {
-        persentation: '100%',
-        icons: 'public/about-icon-smell/swing.svg',
-        title: 'Put Relationships',
-        description: 'We create meaningful experiences that prioritize people their needs.'
-    },
-    {
-        persentation: '150K',
-        icons: 'public/about-icon-smell/cruss.svg',
-        title: 'Data On Records',
-        description: 'We empower best businesses with accurate actionable insights always.'
-    },
-    {
-        persentation: '200+',
-        icons: 'public/about-icon-smell/diamond.svg',
-        title: 'Simplify Workflows',
-        description: 'We reduce complexity so teams can focus on what matters most results.'
-    } 
-]
+  {
+    persentation: "100%",
+    icons: "/icons/iconGuided1.svg",
+    title: "Put Relationships",
+    description:
+      "We create meaningful experiences that prioritize people their needs.",
+  },
+  {
+    persentation: "150K",
+    icons: "/icons/iconGuided2.svg",
+    title: "Data On Records",
+    description:
+      "We empower best businesses with accurate actionable insights always.",
+  },
+  {
+    persentation: "200+",
+    icons: "/icons/iconGuided3.svg",
+    title: "Simplify Workflows",
+    description:
+      "We reduce complexity so teams can focus on what matters most results.",
+  },
+];

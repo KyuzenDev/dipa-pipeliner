6f5900ee99e74fbc96fdfc80d37e05661c9f0f73
KyuzenDev <antcraft517@gmail.com>
2025-06-02 10:06:07 +0700

    refactor(CTA): extract CTA into a standalone component for reusability

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,24 +1,9 @@
-import { Button } from "@/components/ui/button"
-import { Badge } from "@/components/ui/badge"
+import CTA from "@/components/CTA";
+
 export default function Blog() {
     return (
-        <> <div className="max-w-full w-full pt-20 px-16 gap-16">
-            <div className="w-full mx-auto gap-8 items-center flex flex-col">
-                <Badge>Pipeliner Platform</Badge>
-                <div className="flex flex-col gap-5 text-center">
-                    <h1 className="text-4xl font-semibold leading-[120%]">Start Creating Powerful <br /> Relationship Today</h1>
-                    <p className="font-normal text-grayscale-700">
-                        Don&apos;t hesitate to reach out us if you need further assistance.
-                    </p>
-                </div>
-                <div className="flex flex-wrap gap-2 items-center justify-center">
-                    <Button>Start for Free</Button>
-                    <Button variant="secondary">Contact Sales</Button>
-                </div>
-
-                <div className="relative w-[708px] h-[503.48px]">
-                </div>
-            </div>
-        </div></>
+        <>
+        <CTA />
+        </>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,8 @@ import Hero from "@/app/pipeliner-homepage/hero/page";
 import Stats from "@/app/pipeliner-homepage/stats/page";
 import FeaturesDataModel from "@/components/pipeliner-homepage/components/features/featuresDataModel";
 import FeaturesFAQ from "@/components/pipeliner-homepage/components/features/featuresFAQ";
-import {
-  Button
-} from "@/components/ui/button";
-import { Badge } from "@/components/ui/badge";
+import CTA from "@/components/CTA";
+
 export default function RootPage() {
   return (
     <div className="w-full py-12 px-18">
@@ -15,24 +13,7 @@ export default function RootPage() {
       <Stats />
       <FeaturesDataModel />
       <FeaturesFAQ />
-      <div className="max-w-full w-full pt-20 px-16 gap-16">
-        <div className="w-full mx-auto gap-8 items-center flex flex-col">
-          <Badge>Pipeliner Platform</Badge>
-          <div className="flex flex-col gap-5 text-center">
-            <h1 className="text-4xl font-semibold leading-[120%]">Start Creating Powerful <br /> Relationship Today</h1>
-            <p className="font-normal text-grayscale-700">
-              Don&apos;t hesitate to reach out us if you need further assistance.
-            </p>
-          </div>
-          <div className="flex flex-wrap gap-2 items-center justify-center">
-            <Button>Start for Free</Button>
-            <Button variant="secondary">Contact Sales</Button>
-          </div>
-
-          <div className="relative w-[708px] h-[503.48px]">
-          </div>
-        </div>
-      </div>
+      <CTA />
     </div>
   );
 }

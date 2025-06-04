7052bb3f66cd6b98ae0ea7d70f354d378eac5786
Aira Aria <127509175+JihadZaidan@users.noreply.github.com>
2025-06-04 20:06:56 +0700

    ref(ui): added blog content

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,4 +1,5 @@
 import CTA from "@/components/CTA";
+import BlogCard from "@/components/blog/components/blogContent";
 import { Badge } from "@/components/ui/badge";
 
 export default function Blog() {
@@ -9,6 +10,7 @@ export default function Blog() {
                 <h2 className="m-auto w-full font-sans font-semibold text-4xl text-grayscale-900 text-center leading-[120%]">Read Blog & Articles</h2>
                 <p className="m-auto w-full font-sans font-normal text-base text-grayscale-700 text-center leading-[150%]">Learn from our team and top experts on Pipeliner, CRM, and GTM.</p>
             </div>
+            <BlogCard />
             <CTA />
         </>
     )

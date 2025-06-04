8c55422c8add033296d6e0801c65ad15246606a3
Aira Aria <127509175+JihadZaidan@users.noreply.github.com>
2025-06-04 15:26:02 +0700

    styling(ui for header pricing): add header in pricing plan page

--- src/app/pricing-plan/page.tsx
+++ src/app/pricing-plan/page.tsx
@@ -2,10 +2,16 @@ import { pricing } from "@/data/pricing"
 import { Button } from "@/components/ui/button";
 import CTA from "@/components/CTA";
 import FeaturesFAQ from "@/components/pipeliner-homepage/components/features/featuresFAQ";
+import { Badge } from 'lucide-react';
 
 export default function PricingPlan() {
     return (
         <>
+            <div className="max-w-full h-auto border-b border-grayscale-200 pt-32 pb-16 px-16 gap-4 flex flex-col bg-white">
+                <Badge>Pricing</Badge>
+                <h2 className="m-auto w-full font-sans font-semibold text-4xl text-grayscale-900 leading-[120%]">Hack to Unicorn</h2>
+                <p className="m-auto w-full font-sans font-normal text-base text-grayscale-700 leading-[150%]">Start small, dream big. Get going in minutes—no commitment needed.</p>
+            </div>
             <div className="grid grid-cols-1 md:grid-cols-3 p-6 max-w-7xl mx-auto">
                 {pricing.map((plan) => (
                     <div

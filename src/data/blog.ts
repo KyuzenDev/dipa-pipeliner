62cfa2da06558ec55fa7ebefaf0b14f17e3ae321
Aira Aria <127509175+JihadZaidan@users.noreply.github.com>
2025-06-05 08:41:47 +0700

    ref(ui): setting files name

--- src/data/blog.ts
+++ src/data/blog.ts
@@ -1,7 +1,7 @@
 import { BlogDetail } from "@/app/blog/types/blogDetail";
 export const blogDetail: BlogDetail[] = [
   {
-    photos: "@/public/CMS/CMS-1.svg",
+    photos: "@/CMS-1.svg",
     date: "April 28, 2025",
     category: "Product",
     title: "How to Choose the Right CRM for Your Personal and Business",
@@ -9,7 +9,7 @@ export const blogDetail: BlogDetail[] = [
       "Not all CRMs are created equal. Here’s how to pick the best one for your needs.",
   },
   {
-    photos: "@/public/CMS/CMS-2.svg",
+    photos: "@/CMS-2.svg",
     date: "April 26, 2025",
     category: "Companies",
     title: "The Future of CRM: Trends to Now Companies Watch in 2025",
@@ -17,7 +17,7 @@ export const blogDetail: BlogDetail[] = [
       "Stay ahead of the game with the latest trends shaping customer relationship tools.",
   },
   {
-    photos: "@/public/CMS/CMS-3.svg",
+    photos: "@/CMS-3.svg",
     date: "April 24, 2025",
     category: "Customers",
     title: "How Sales Teams Use CRM to Close Deals with Customers",
@@ -25,7 +25,7 @@ export const blogDetail: BlogDetail[] = [
       "Not all CRMs are created equal. Here’s how to pick the best one for your needs.",
   },
   {
-    photos: "@/public/CMS/CMS-4.svg",
+    photos: "@/CMS-4.svg",
     date: "April 22, 2025",
     category: "Product",
     title: "CRM 101: A Beginner’s Guide to Customer Relationship Team",
@@ -33,7 +33,7 @@ export const blogDetail: BlogDetail[] = [
       "New to CRM? Here's a quick guide to what it is, how it works, and why it matters.",
   },
   {
-    photos: "@/public/CMS/CMS-5.svg",
+    photos: "@/CMS-5.svg",
     date: "April 20, 2025",
     category: "Companies",
     title: "CRM for Best Marketing Teams What You Need to Fast Know",
@@ -41,7 +41,7 @@ export const blogDetail: BlogDetail[] = [
       "See how marketing can use CRM to improve targeting, timing, and tracking.",
   },
   {
-    photos: "@/public/CMS/CMS-6.svg",
+    photos: "@/CMS-6.svg",
     date: "April 18, 2025",
     category: "Customers",
     title: "How Founders Use CRM to Stay Close to Good Value Customers",
@@ -49,7 +49,7 @@ export const blogDetail: BlogDetail[] = [
       "Tips for startup founders to use CRM for building lasting relationships.",
   },
   {
-    photos: "@/public/CMS/CMS-7.svg",
+    photos: "@/CMS-7.svg",
     date: "April 10, 2025",
     category: "Product",
     title: "How to Clean Your CRM Data (Without the Headache More)",
@@ -57,7 +57,7 @@ export const blogDetail: BlogDetail[] = [
       "Keep your CRM accurate and useful with easy data-cleaning tips.",
   },
   {
-    photos: "@/public/CMS/CMS-8.svg",
+    photos: "@/CMS-8.svg",
     date: "April 10, 2025",
     category: "Companies",
     title: "See Real-Life CRM Setups from Good Modern Pipeliner Teams",
@@ -65,7 +65,7 @@ export const blogDetail: BlogDetail[] = [
       "Inspiration from how real startups and companies set up and use their CRMs.",
   },
   {
-    photos: "@/public/CMS/CMS-9.svg",
+    photos: "@/CMS-9.svg",
     date: "April 10, 2025",
     category: "Customers",
     title: "Customer Success and CRM: A Perfect Match with Piperliner",

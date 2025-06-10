22d095c2a61bf20d2e3bf852392face51c531eed
Aira Aria <127509175+JihadZaidan@users.noreply.github.com>
2025-06-10 10:01:21 +0700

    ref(ui) and styling : font badge variant

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -12,7 +12,7 @@ const badgeVariants = cva(
         default:
           "border-1 border-grayscale-200 rounded-full px-4 py-2 bg-white text-secondary-foreground",
         secondary:
-          "border-transparent pl-1 pr-2 py-0.5 rounded-xl bg-gradient-to-b from-[#ECF0F190] to-[#FFFFFF90] text-secondary-foreground",
+          "border-transparent pl-1 pr-2 py-0.5 rounded-xl bg-gradient-to-b from-[#ECF0F190] to-[#FFFFFF90] text-secondary-foreground text-grayscale-900 text-sm font-medium",
         blue:
           "border-transparent px-2 py-0.5 rounded-xl bg-gradient-to-b from-[#FFFFFF] to-[#B7E2FA] text-secondary-foreground",
         outline:

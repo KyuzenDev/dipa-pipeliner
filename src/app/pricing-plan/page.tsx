f6e00c19d9f466ec456cb39739edb27cdd1d5d0a
Aira Aria <127509175+JihadZaidan@users.noreply.github.com>
2025-06-11 09:44:55 +0700

    styling : Set position and added image to pricing header

--- src/app/pricing-plan/page.tsx
+++ src/app/pricing-plan/page.tsx
@@ -3,6 +3,8 @@ import { Button } from "@/components/ui/button";
 import CTA from "@/components/CTA";
 import FeaturesFAQ from "@/components/pipeliner-homepage/components/features/featuresFAQ";
 import { Badge } from "@/components/ui/badge";
+import Image from "next/image";
+import Plate from "../../../public/pricing-plan/head.svg"
 
 export default function PricingPlan() {
     return (
@@ -11,6 +13,8 @@ export default function PricingPlan() {
                 <Badge>Pricing</Badge>
                 <h2 className="m-auto w-full font-sans font-semibold text-4xl text-grayscale-900 leading-[120%]">Hack to Unicorn</h2>
                 <p className="m-auto w-full font-sans font-normal text-base text-grayscale-700 leading-[150%]">Start small, dream big. Get going in minutes—no commitment needed.</p>
+
+                <Image src={Plate} alt="photos" className="absolute right-0 bottom-0"></Image>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-3 p-6 max-w-7xl mx-auto">
                 {pricing.map((plan) => (

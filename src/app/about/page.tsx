42e4737f11eed43f0d114899d0dd0ceccecbad42
Aira Aria <127509175+JihadZaidan@users.noreply.github.com>
2025-06-11 10:10:16 +0700

    ref: added ceo testimony to about us

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,6 +3,8 @@ import GuidedCard from "@/components/about/components/card/guidedCard";
 import { Badge } from "@/components/ui/badge";
 import FeaturesFAQ from "@/components/pipeliner-homepage/components/features/featuresFAQ";
 import CTA from "@/components/CTA";
+import Image from "next/image";
+import Captain from "../../../public/Relationship/ceo.svg"
 
 export default function About() {
     return (
@@ -30,6 +32,22 @@ export default function About() {
                             Don&apos;t hesitate to reach out to us if you need further assistance.
                         </p>
                         <GuidedCard />
+                    </div>
+                </div>
+                <div className="max-w-full w-full h-[390px] bg-white px-15 border-y border-grayscale-200">
+                    <div className="m-auto w-full h-full border-x px-15 py-20 gap-10 bg-grayscale-200 border-grayscale-200 flex flex-row justify-center items-center">
+                        <Image src={Captain} alt="ceo-founder"></Image>
+                        <div className="w-[630px] h-[205px] gap-14 mt-[-30px] flex flex-col">
+                            <p className="w-full h-[126px] font-sans font-normal text-4xl text-grayscale-900 leading-[140%]">
+                                Pipeliner is the first CRM that feels truly 
+                                modern. It’s powerful, flexible, and fast
+                                to build with. There’s nothing like it.
+                            </p>
+                            <div className="w-[155px] h-[45px] gap-1 flex flex-col">
+                                <p className="font-sans font-semibold text-base leading-[150%] text-grayscale-900">Odessa Mira</p>
+                                <p className="w-full font-sans font-normal text-sm leading-[150%] text-grayscale-600">CEO & Head of Finance</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className="w-full py-20 h-auto gap-8 flex flex-col items-center mx-auto">

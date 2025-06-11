eb2f17ed91c90fbc03913e2c1408aa59ed94bb31
KyuzenDev <antcraft517@gmail.com>
2025-06-11 15:43:59 +0700

    style(about): improve FAQ and Guided section styling for better readability and UX

--- src/components/pipeliner-homepage/components/features/featuresFAQ.tsx
+++ src/components/pipeliner-homepage/components/features/featuresFAQ.tsx
@@ -34,7 +34,7 @@ export default function FeaturesFAQ() {
               <AccordionTrigger className="cursor-pointer">{item.title}</AccordionTrigger>
               <AccordionContent className="flex flex-col gap-5">
                 {item.content}
-                <div className="border-1 text-grayscale-300 border-dashed"></div>
+                <div className="border-t-2 border-dashed border-grayscale-200 pb-4"></div>
                 <Link
                   href={item.link}
                   className="text-sm text-grayscale-600 flex items-center justify-between gap-1 cursor-pointer group"

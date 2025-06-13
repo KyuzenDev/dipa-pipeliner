4ddeff1875b810bfc8862cafd5018b031fe48368
Aira Aria <127509175+JihadZaidan@users.noreply.github.com>
2025-06-13 14:18:11 +0700

    ref : tablet responsive position

--- src/components/blog/components/blogContent.tsx
+++ src/components/blog/components/blogContent.tsx
@@ -4,25 +4,31 @@ import Image from "next/image";
 
 export default function BlogCard() {
     return (
-        <div className="max-w-full w-full px-15 py-12 gap-2 justify-center items-center">
-            <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-3 gap-8">
+        <div className="w-full py-12 px-4 flex flex-col items-center">
+            <div className="w-full max-w-7xl grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
                 {blogDetail.map((item, index) => (
                     <div key={index} className="space-y-3">
-                        <Image 
-                            src={`/CMS/CMS-${index + 1}.svg`} 
-                            width={303} 
-                            height={202} 
-                            alt={item.title} 
-                            className="rounded-lg w-full h-52 object-cover" 
+                        <Image
+                            src={`/CMS/CMS-${index + 1}.svg`}
+                            width={400}
+                            height={250}
+                            alt={item.title}
+                            className="rounded-lg w-full h-52 object-cover"
                         />
-                        <div className="text-sm text-gray-500">{item.date} | {item.category}</div>
-                        <h3 className="text-lg font-semibold text-gray-900 cursor-pointer hover:text-blue-300">{item.title}</h3>
+                        <div className="text-sm text-gray-500">
+                            {item.date} | {item.category}
+                        </div>
+                        <h3 className="text-lg font-semibold text-gray-900 cursor-pointer hover:text-blue-400">
+                            {item.title}
+                        </h3>
                         <p className="text-gray-600 text-sm">{item.description}</p>
                     </div>
                 ))}
             </div>
 
-            <Button variant="secondary" className="m-auto">Load More Blogs</Button>
+            <Button variant="secondary" className="mt-10">
+                Load More Blogs
+            </Button>
         </div>
     );
 }

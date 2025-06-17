11c1bd9b53c0fa12289174880e29b59fc20d6b4f
Zenitsu Zidane <127509175+JihadZaidan@users.noreply.github.com>
2025-06-17 21:20:25 +0700

    zero

--- src/components/pipeliner-homepage/components/features/featuresTesti.tsx
+++ src/components/pipeliner-homepage/components/features/featuresTesti.tsx
@@ -1,13 +1,1 @@
-import { Badge } from "@/components/ui/badge";
 
-export default function Testimony() {
-    <div className="max-w-full h-auto block static px-16 py-20 bg-[#F5F8FC] justify-center items-center">
-        <div className="w-full h-auto static flex flex-col gap-16 bg-transparent">
-            <div className="w-full h-auto static flex flex-col gap-4 justify-center items-center text-center">
-                <Badge variant="default">Testimonials</Badge>
-                <h2 className="font-sans font-semibold text-[40px] text-center text-grayscale-900">Their Favorites Feature</h2>
-                <p className="font-sans font-normal text-base text-center text-grayscale-700">Leverage insights from your business, customer, and product data <br /> to drive and enhance your team's performance and success.</p>
-            </div>
-        </div>
-    </div>
-}

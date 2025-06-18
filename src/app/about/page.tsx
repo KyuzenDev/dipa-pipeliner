bab06ac976c03fae2c56bdded0a4c5d66bc339de
KyuzenDev <antcraft517@gmail.com>
2025-06-18 10:56:58 +0700

    style(about/hero): enhance visual styling and layout responsiveness in hero section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,8 +8,9 @@ import Image from "next/image";
 export default function About() {
     return (
         <>
-            <div className="max-w-full w-full h-auto pt-12 px-4 md:px-[64px] gap-2">
-                <div className="relative w-full gap-4 justify-between items-center flex flex-col mx-auto mb-52">
+            <div className="absolute w-[610px] -z-5 h-64 bg-[linear-gradient(to_top_right,_#88CCF8,_#D9F8C0)] rounded-xl blur-[160px]"></div>
+            <div className="max-w-full w-full h-auto pt-12 gap-2">
+                <div className="relative w-full gap-4 justify-between items-center flex flex-col mx-auto pb-52">
                     <Badge variant="default">
                         About
                     </Badge>
@@ -21,31 +22,36 @@ export default function About() {
                         Pipeliner is on a mission to craft tools that support business growth through smart.
                     </p>
 
-                    <div className="absolute -bottom-8">
-                        <Image
-                            src="/ornament.svg"
-                            alt="Ornament Image"
-                            width={718}
-                            height={347}
-                            className="w-full h-auto object-cover"
-                        />
-                    </div>
+                    
                     <div className="relative z-10">
                         <Image
-                            src="/imageAbout.svg"
+                            src="/imageAbout.png"
                             alt="About Hero"
                             width={539}
                             height={329}
                             className="w-full h-auto object-cover"
                         />
+
+                        <div className="absolute top-0 left-1/2 -translate-x-1/2 w-[750px]">
+                            <Image
+                                src="/ornament.svg"
+                                alt="Ornament Image"
+                                width={750}
+                                height={400}
+                                className="w-full h-auto object-contain"
+                            />
+                        </div>
                     </div>
+
+
+                    <div className="absolute w-[610px] -z-5 h-64 bg-[linear-gradient(to_top_right,_#D9F8C0,_#88CCF8)] bottom-0 right-0 rounded-xl blur-[160px]"></div>
                 </div>
-                <div className="w-full h-auto flex justify-center items-center px-8">
+                <div className="w-full h-auto flex justify-center items-center py-8 px-8">
                     <div className="w-full max-w-7xl py-8 flex justify-center items-center">
                         <Image src="/hero/pipeliner.png" alt="Pipeliner" width={1400} height={32} />
                     </div>
                 </div>
-                <div className="w-full py-28 h-auto gap-16 flex flex-col items-between mx-auto">
+                <div className="w-full px-4 md:px-[64px] py-28 h-auto gap-16 flex flex-col items-between mx-auto">
                     <div className="w-full h-auto gap-4 flex flex-col justify-between items-center text-center">
                         <Badge variant="default">
                             Guided
@@ -57,7 +63,7 @@ export default function About() {
                     </div>
                     <GuidedCard />
                 </div>
-                <div className="w-full h-auto flex flex-col items-center mx-auto">
+                <div className="w-full px-4 md:px-[64px] h-auto flex flex-col items-center mx-auto">
                     <div className="w-full bg-[#F5F8FC] flex justify-center items-center border border-grayscale-200 py-10 px-5">
                         <div className="w-full max-w-7xl flex flex-col lg:flex-row justify-center items-center gap-10 relative">
                             <div className="w-[203px] h-[230px] relative bg-gradient-to-br from-[#D9F8C0] to-[#88CCF8] border-[10px] border-white rounded-lg p-5">
@@ -84,7 +90,7 @@ export default function About() {
                         </div>
                     </div>
                 </div>
-                <div className="w-full py-20 h-auto gap-8 flex flex-col items-center mx-auto">
+                <div className="w-full px-4 md:px-[64px] py-20 h-auto gap-8 flex flex-col items-center mx-auto">
                     <div className="w-full h-auto gap-4 flex flex-col justify-between items-center text-center">
                         <Badge variant="default">
                             Founder
@@ -96,7 +102,6 @@ export default function About() {
                         <CEO />
                     </div>
                 </div>
-
             </div>
             <FeaturesFAQ />
             <CTA />

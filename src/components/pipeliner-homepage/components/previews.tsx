bfc8cdd6c0022b7bc67c6decd088e602b09ba44e
KyuzenDev <antcraft517@gmail.com>
2025-06-18 13:51:27 +0700

    fix(deploy): resolve deployment error due to incorrect build configuration

--- src/components/pipeliner-homepage/components/previews.tsx
+++ src/components/pipeliner-homepage/components/previews.tsx
@@ -2,7 +2,7 @@
 
 import Image from 'next/image';
 import { useState } from 'react';
-import { Pagination } from '../ui/pagination';
+import {Pagination} from '@/components/ui/pagination';
 
 type User = {
   id: number;

0dc0523d932ed829a0b69a5df98963869749c5b0
Zenitsu Zidane <127509175+JihadZaidan@users.noreply.github.com>
2025-06-18 13:32:51 +0700

    ref : add testimony

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import FeaturesCRM from "@/components/pipeliner-homepage/components/features/fea
 import Hero from "@/app/pipeliner-homepage/hero/page";
 import Stats from "@/app/pipeliner-homepage/stats/page";
 import FeaturesDataModel from "@/components/pipeliner-homepage/components/features/featuresDataModel";
+import Testimony from "@/components/pipeliner-homepage/components/features/featuresTesti";
 import FeaturesFAQ from "@/components/pipeliner-homepage/components/features/featuresFAQ";
 import CTA from "@/components/CTA";
 
@@ -12,6 +13,7 @@ export default function RootPage() {
       <FeaturesCRM />
       <Stats />
       <FeaturesDataModel />
+      <Testimony/>
       <FeaturesFAQ />
       <CTA />
     </div>

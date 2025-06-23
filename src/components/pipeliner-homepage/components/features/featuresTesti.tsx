89adb6222556a3ac46092fef27e246184442dd90
GivoDev <127509175+JihadZaidan@users.noreply.github.com>
2025-06-23 14:36:33 +0700

    ref : setting size font and set position responsive

--- src/components/pipeliner-homepage/components/features/featuresTesti.tsx
+++ src/components/pipeliner-homepage/components/features/featuresTesti.tsx
@@ -15,21 +15,21 @@ export default function Testimony() {
                     <h2 className="font-sans font-semibold text-[40px] text-center text-grayscale-900">Their Favorites Feature</h2>
                     <p className="font-sans font-normal text-base text-center text-grayscale-700">Leverage insights from your business, customer, and product data <br /> to drive and enhance your team&apos;s performance and success.</p>
                 </div>
-                <div className="w-full h-auto static flex flex-row gap-5">
-                    <div className="w-full h-[450px] static flex flex-col p-[40px] rounded-xl gap-[40px] bg-[#EBEDFD]">
-                        <h3 className="font-sans font-normal text-3xl leading-[140%] text-grayscale-900">
+                <div className="w-full h-auto static flex flex-col lg:flex-row gap-5">
+                    <div className="w-full h-auto static flex flex-col p-6 sm:p-10 rounded-xl gap-6 bg-[#EBEDFD] border-10 border-white">
+                        <h3 className="font-sans font-normal text-xl sm:text-2xl md:text-3xl leading-[140%] text-grayscale-900">
                             Pipeliner is the first CRM that feels truly modern. It&apos;s powerful, flexible, and fast to build with. There&apos;s nothing like it.
                         </h3>
-                        <div className="w-full h-auto static flex flex-row gap-4">
-                            <Image src={samsul} alt="photo" className="w-13 h-13"></Image>
-                            <div className="w-full static gap-1 flex flex-col">
-                                <p className="w-full font-sans font-semibold text-base text-grayscale-900 leading-[150%]">Sahil Mansuri</p>
-                                <p className="w-full font-sans font-normal text-base text-grayscale-600 leading-[150%]">CEO & Head of Finance</p>
+                        <div className="w-full h-auto static flex items-center gap-4">
+                            <Image src={samsul} alt="photo" className="w-13 h-13 rounded-full"></Image>
+                            <div className="flex flex-col">
+                                <p className="font-sans font-semibold text-base text-grayscale-900 leading-[150%]">Sahil Mansuri</p>
+                                <p className="font-sans font-normal text-sm text-grayscale-600 leading-[150%]">CEO & Head of Finance</p>
                             </div>
                         </div>
-                        <div className="border-1 text-grayscale-300 border-dashed"></div>
+                        <div className="border-t border-dashed border-grayscale-300"></div>
                         <div className="w-full h-auto flex flex-col"></div>
-                        <div className="w-full h-auto static flex flex-col bg-transparent gap-4 mt-[-40px]">
+                        <div className="w-full h-auto static flex flex-col bg-transparent gap-4 mt-[5px]">
                             <p className="w-full h-auto font-sans font-normal text-sm text-grayscale-600">Favorites Future</p>
                             <div className="w-full h-auto static flex flex-row gap-4">
                                 <div className="w-30 h-auto static flex flex-row gap-2">

3ef8489e3b1d83c195657b87e96209a61c930e67
GivoDev <127509175+JihadZaidan@users.noreply.github.com>
2025-06-23 11:46:45 +0700

    Add files via upload

---
+++ src/components/blog/components/blog-content/six.tsx
@@ -0,0 +1,24 @@
+import CTA from "@/components/CTA";
+import SubBlog from "../blogdetails/headerblogdetail";
+import Image from "next/image";
+import ContentBlogDetail from "../blogdetails/contentblogdetail";
+import BlogCard from "../blogContent";
+import Six from "../../../../../public/content-of-blog/blog-6.png"
+
+export default function SixPage() {
+    return (
+        <section className="max-w-full w-full">
+            <div className="w-full border-b border-white py-15 px-15 gap-10 flex flex-col justify-center items-center">
+                <SubBlog
+                    title="How Sales Teams Use CRM to Close Deals with Customers"
+                    date="April 24, 2025"
+                    detail="Customers"
+                />
+                <Image src={Six} alt="photos"></Image>
+                <ContentBlogDetail/>
+                <BlogCard/>
+            </div>
+            <CTA />
+        </section>
+    )
+}

07aa6e830ed7d0c75c95bf843874938d976c757f
Wish Code <127509175+JihadZaidan@users.noreply.github.com>
2025-07-02 09:02:02 +0700

    ref : rename class function

--- src/components/blog/components/blogContent.tsx
+++ src/components/blog/components/blogContent.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function BlogCard() {
+export default function BlogList() {
     return (
         <div className="w-full py-12 px-4 flex flex-col items-center">
             <div className="w-full max-w-7xl grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">

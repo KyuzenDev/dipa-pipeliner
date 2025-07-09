cea7e3720ca37a73a8c0fcb5937f0af99ea3410e
Jazzyra <127509175+JihadZaidan@users.noreply.github.com>
2025-07-09 11:34:31 +0700

    feat(breakpoint): intialy Next App breakpoint

--- src/hooks/use-LandscapeTablet.ts
+++ src/hooks/use-LandscapeTablet.ts
@@ -1,17 +1,17 @@
 import * as React from "react"
 
-const TABLET_BREAKPOINT = 992
+const LANDSCAPE_TABLET_BREAKPOINT = 1024
 
 export function useIsTablet() {
   const [isTablet, setIsTablet] = React.useState<boolean | undefined>(undefined)
 
   React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${TABLET_BREAKPOINT - 1}px)`)
+    const mql = window.matchMedia(`(max-width: ${LANDSCAPE_TABLET_BREAKPOINT - 1}px)`)
     const onChange = () => {
-      setIsTablet(window.innerWidth < TABLET_BREAKPOINT)
+      setIsTablet(window.innerWidth < LANDSCAPE_TABLET_BREAKPOINT)
     }
     mql.addEventListener("change", onChange)
-    setIsTablet(window.innerWidth < TABLET_BREAKPOINT)
+    setIsTablet(window.innerWidth < LANDSCAPE_TABLET_BREAKPOINT)
     return () => mql.removeEventListener("change", onChange)
   }, [])
 

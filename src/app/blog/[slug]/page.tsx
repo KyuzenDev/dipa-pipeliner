06b31f6cba464fd32146f0306be88e7b1c7bc6a2
KyuzenDev <antcraft517@gmail.com>
2025-07-10 09:25:06 +0700

    fix: adjust PageProps interface for blog slug

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -3,13 +3,12 @@ import { blogBySlugQuery } from "@/lib/queries";
 import { notFound } from "next/navigation";
 import Image from "next/image";
 import { PortableText } from "@portabletext/react";
-type BlogParams = {
+export default async function BlogDetailPage({
+  params,
+}: {
   params: { slug: string };
-};
-
-export default async function BlogDetailPage({ params }: BlogParams) {
+}) {
   const blog = await client.fetch(blogBySlugQuery, { slug: params.slug });
-
   if (!blog) return notFound();
 
   return (

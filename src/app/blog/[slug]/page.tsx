f706f50f24a61bc5bbfb52e866345cf226f30dc6
Jazzyra <127509175+JihadZaidan@users.noreply.github.com>
2025-07-16 11:33:52 +0700

    feat(detail): minor solve social media button structure expl

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -41,29 +41,32 @@ export default async function BlogDetailPage({
       <p className="text-base text-gray-700">{blog.description}</p>
       <PortableText value={blog.body} />
 
-      <div className="flex justify-left items-left md:flex gap-1 md:justify-left md:items-left">
-            {blogdetailSocialMedia.map(({ platform, url }) => (
-              <Link
-                href={url}
-                key={platform}
-                target="_blank"
-                rel="noopener noreferrer"
+      <div className="max-w-full flex flex-col justify-left items-left md:flex gap-3 md:justify-left md:items-left">
+        <p className="text-base leading-[150%] text-gray-600">Share post on</p>
+        <div className="flex justify-left items-left md:flex gap-1 md:justify-left md:items-left">
+          {blogdetailSocialMedia.map(({ platform, url }) => (
+            <Link
+              href={url}
+              key={platform}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Button
+                size="icon"
+                className="nav-item w-7 h-7 p-1 relative transition-none flex cursor-pointer justify-center items-center rounded bg-grayscale-200 hover:bg-gray-300"
               >
-                <Button
-                  size="icon"
-                  className="nav-item w-7 h-7 p-1 relative transition-none flex cursor-pointer justify-center items-center rounded bg-grayscale-200 hover:bg-gray-300"
-                >
-                  <Image
-                    src={`/socialMedia/${platform}.svg`}
-                    alt={platform}
-                    width={16}
-                    height={16}
-                    className="absolute z-1"
-                  />
-                </Button>
-              </Link>
-            ))}
-          </div>
+                <Image
+                  src={`/socialMedia/${platform}.svg`}
+                  alt={platform}
+                  width={16}
+                  height={16}
+                  className="absolute z-1"
+                />
+              </Button>
+            </Link>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }

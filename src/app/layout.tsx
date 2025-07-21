6ca96a646fe57df891f9e87caaef3302c90e65f2
Jazzyra <127509175+JihadZaidan@users.noreply.github.com>
2025-07-21 11:09:08 +0700

    feat: update auto layout CTA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import TopNavbar from "@/components/TopNavbar";
 import Footer from "@/components/Footer";
 import { usePathname } from "next/navigation";
 import Head from "next/head";
+import CTA from "@/components/CTA";
 
 const inter = Inter_Tight({
   subsets: ["latin"],
@@ -36,6 +37,7 @@ export default function Layout({
           )}
           <TopNavbar />
           <div className="w-full">{children}</div>
+          <CTA/>
           <Footer />
         </main>
       </body>

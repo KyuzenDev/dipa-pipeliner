ed5cb62cef722673cf3b7efcbda4ee66b71be31a
KyuzenDev <antcraft517@gmail.com>
2025-07-21 11:07:28 +0700

    feat(blog-detail): improve content hierarchy and navigation with Blog back link

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -3,7 +3,7 @@ import { blogBySlugQuery } from "@/lib/queries";
 import { notFound } from "next/navigation";
 import Image from "next/image";
 import { PortableText } from "@portabletext/react";
-
+import Link from "next/link";
 export default async function BlogDetailPage({
   params,
 }: {
@@ -24,6 +24,25 @@ export default async function BlogDetailPage({
 
   return (
     <div className="max-w-3xl mx-auto py-10 px-6 space-y-6">
+      <Link
+        href="/blog"
+        className="flex items-center gap-2 text-grayscale-900 hover:text-grayscale-600 text-sm font-medium transition"
+      >
+        <Image
+          width={16}
+          height={16}
+          alt="Back to blog"
+          src="/icons/flip-backward.svg"
+        />
+        Blog
+      </Link>
+
+      <h1 className="text-4xl font-semibold">{blog.title}</h1>
+      <div className="flex gap-3">
+        <p className="text-gray-500 text-normal">{formatDate(blog.publishedAt)}</p>
+        <div className="border-1 text-grayscale-600 opacity-20"></div>
+        <p className="text-gray-500 text-normal">{blog.category}</p>
+      </div>
       <Image
         src={blog.coverImage.asset.url}
         alt={blog.title}
@@ -31,10 +50,6 @@ export default async function BlogDetailPage({
         height={500}
         className="rounded-xl object-cover w-full h-auto"
       />
-      <h1 className="text-3xl font-bold">{blog.title}</h1>
-      <p className="text-gray-500 text-sm">
-        {formatDate(blog.publishedAt)} | {blog.category}
-      </p>
       <p className="text-base text-gray-700">{blog.description}</p>
       <PortableText value={blog.body} />
     </div>

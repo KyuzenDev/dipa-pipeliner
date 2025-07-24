6e031d3878c460cf26f925643eda6e629aa546bb
KyuzenDev <antcraft517@gmail.com>
2025-07-24 12:36:13 +0700

    style(more-insight): improve layout, spacing, and hover effects; add CTA component to blog detail

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -62,11 +62,27 @@ export default async function BlogDetailPage({
           height={500}
           className="rounded-xl object-cover w-full h-auto"
         />
-        <p className="text-base text-gray-700">{blog.description}</p>
+        <p className="text-lg text-gray-700">{blog.description}</p>
         {blog.body ? (
           <PortableText value={blog.body} />
         ) : (
-          <p className="text-gray-400">No content available yet.</p>
+          <div className="flex flex-col gap-4">
+            <h4 className="font-sans font-semibold text-2xl leading-[140%] text-gray-900">
+              The Game Changer For CRM
+            </h4>
+            <p className="font-sans font-normal text-lg leading-[160%] text-gray-600">
+              By 2025, AI-powered CRMs won’t just store data they’ll write
+              follow-ups, summarize meetings, track interactions, and predict
+              customer needs. What was once a bonus will soon be essential.
+              These AI-driven features will shift from nice-to-have.
+            </p>
+            <p className="font-sans font-normal text-lg leading-[160%] text-gray-600">
+              Think about it automated meeting summaries, email drafting, and
+              activity tracking will soon be the new standard. This means sales
+              teams can say goodbye to hours spent on repetitive tasks like data
+              entry and instead focus on selling.
+            </p>
+          </div>
         )}
 
         <div className="max-w-full flex flex-col justify-left items-left md:flex gap-3 md:justify-left md:items-left">
